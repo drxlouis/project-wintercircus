f97fade430ae2b2d8cd349d707e5983b771d45d3
Louis Dierickx <dierickx.louis01@gmail.com>
2025-05-19 14:10:24 +0200

    d

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/project-wintercirucs/",
+  base: "/project-wintercircus/",
   build: {
     outDir: "docs",
   },
