ebf4098f7f305c065966c2e5fc80c57fa702e443
Louis Dierickx <dierickx.louis01@gmail.com>
2025-05-19 13:57:16 +0200

    d

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/project-wintercirucs/", // Ensure this matches your GitHub Pages repo name
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-  },
+  base: "/project-wintercirucs/",
 });
