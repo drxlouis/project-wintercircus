7b65710924864e9730fbab578a58456a50b43342
Louis Dierickx <dierickx.louis01@gmail.com>
2025-05-19 13:37:57 +0200

    index moved

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/project-wintercircus/",
+  base: "/project-wintercirucs/",
+  root: "src", // Set the root directory to "src"
   plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: "../dist", // Adjust output directory since "src" is now the root
     assetsDir: "assets",
     rollupOptions: {
       output: {
